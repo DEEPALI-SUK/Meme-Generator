271a6839816ab747368ec1cc38daf5a84f383c4a
DEEPALI-SUK <64724041+DEEPALI-SUK@users.noreply.github.com>
2020-08-15 22:00:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from "./Head"
+import MemeG from "./MemeG"
+import './App.css';
+function App(){
+  return(
+      <div>
+        <Head />
+        <MemeG />
+      </div>
+  )
+}
+
+
+
+export default App;
---
+++ src/Head.js
@@ -0,0 +1,11 @@
+import React from "react"
+function Head() {
+    return(
+        <header>
+            
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Head
